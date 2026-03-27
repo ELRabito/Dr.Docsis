d1feaa8e9b7e47d37a9948516ff7ab216776af45
ELRabito <39779934+ELRabito@users.noreply.github.com>
2026-03-27 03:55:15 +0100

    Updated Hosts

--- iPerf3_Host_Config.ps1
+++ iPerf3_Host_Config.ps1
@@ -7,9 +7,10 @@ $targets = @(
     @{ Country = "Switzerland";     Host = "t5.cscs.ch";                     Ports = 5201..5203 }
     @{ Country = "Switzerland";     Host = "speedtest.iway.ch";              Ports = 5201 }
     @{ Country = "Switzerland";     Host = "speedtest.shinternet.ch";        Ports = 5200..5209 }
+	@{ Country = "Switzerland";     Host = "speedtest.init7.net";        	 Ports = 5201..5204 }
     @{ Country = "France";          Host = "ping.online.net";                Ports = 5200..5209 }
+	@{ Country = "France";          Host = "ping-90ms.online.net";           Ports = 5200..5209 }
 	@{ Country = "Netherlands";     Host = "iperf-ams-nl.eranium.net";       Ports = 5201..5210 }
-    @{ Country = "Netherlands";     Host = "speedtest.ams1.novogara.net";    Ports = 5200..5209 }
     @{ Country = "Denmark";         Host = "speed1.fiberby.dk";              Ports = 9201..9240 }
     @{ Country = "Denmark";         Host = "speed2.fiberby.dk";              Ports = 9201..9240 }
     @{ Country = "Norway";          Host = "lg.gigahost.no";                 Ports = 9201..9240 }
