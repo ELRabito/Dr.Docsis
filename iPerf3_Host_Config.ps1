df9eef9fe691c1463e313037ab3761a3e9cd575b
ELRabito <39779934+ELRabito@users.noreply.github.com>
2026-04-22 18:04:35 +0200

    Removed unreliably host entry from config

--- iPerf3_Host_Config.ps1
+++ iPerf3_Host_Config.ps1
@@ -9,7 +9,6 @@ $targets = @(
     @{ Country = "Switzerland";     Host = "speedtest.shinternet.ch";        Ports = 5200..5209 }
 	@{ Country = "Switzerland";     Host = "speedtest.init7.net";        	 Ports = 5201..5204 }
     @{ Country = "France";          Host = "ping.online.net";                Ports = 5200..5209 }
-	@{ Country = "France";          Host = "ping-90ms.online.net";           Ports = 5200..5209 }
 	@{ Country = "Netherlands";     Host = "iperf-ams-nl.eranium.net";       Ports = 5201..5210 }
     @{ Country = "Denmark";         Host = "speed1.fiberby.dk";              Ports = 9201..9240 }
     @{ Country = "Denmark";         Host = "speed2.fiberby.dk";              Ports = 9201..9240 }
